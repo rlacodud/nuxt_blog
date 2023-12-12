c4a74dffd7185647eed5e9ed4d41af28974aab9e
young <young@spadecompany.kr>
2023-12-12 13:28:05 +0900

    feat(SignUp): 레이아웃 작업

--- pages/SignUp.vue
+++ pages/SignUp.vue
@@ -1,10 +1,24 @@
-<script setup>
-
-</script>
-
 <template>
-
+  <div class="container sign_container">
+    <div class="title_container">
+      <h1>Sign up</h1>
+      <nuxt-link to="/signin">Have an account?</nuxt-link>
+    </div>
+    <div class="input_container">
+      <form>
+        <Input type="text" id="signUpUsername" placeholder="Username" :required="true"/>
+        <Input type="email" id="signUpEmail" placeholder="Email" :required="true"/>
+        <Input type="password" id="signUpPassword" placeholder="Password" :required="true"/>
+        <Button class="flex" title="Sign up" is-class="btn-primary" size="btn-l"/>
+      </form>
+    </div>
+  </div>
 </template>
+
+<script setup>
+import Input from "@/components/common/Input.vue";
+import Button from "@/components/common/Button.vue";
+</script>
 
 <style scoped>
 
