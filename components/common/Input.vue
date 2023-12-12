832a6456c9e1f78347bd09e37d07e7f872f8f694
young <young@spadecompany.kr>
2023-12-12 13:07:46 +0900

    feat: contents.css | Input component 생성

---
+++ components/common/Input.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="input_box">
+    <label :for="id" v-show="isLabel">{{label}}</label>
+    <input class="input"
+      :id="id"
+      :placeholder="placeholder"
+      :value="value"
+      :readonly="readonly"
+      :disabled="disabled"
+    />
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  id: {
+    type: String,
+    required: true
+  },
+  isLabel: {
+    type: Boolean,
+    default: false
+  },
+  label: String,
+  placeholder: String,
+  value: String,
+  readonly: Boolean,
+  disabled: Boolean
+})
+</script>
+
+<style scoped>
+
+</style>
