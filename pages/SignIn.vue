db0351e72f88de52d850de27404e511e4e85b805
young <young@spadecompany.kr>
2023-12-12 13:27:36 +0900

    chore(SignIn): props 추가 | class명 변경

--- pages/SignIn.vue
+++ pages/SignIn.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="container sign-in_container">
+  <div class="container sign_container">
     <div class="title_container">
       <h1>Sign in</h1>
       <nuxt-link to="/signup">Need an account?</nuxt-link>
     </div>
     <div class="input_container">
       <form>
-        <Input id="email" placeholder="Email" :required="true"/>
-        <Input id="password" placeholder="Password" :required="true"/>
+        <Input type="email" id="signInEmail" placeholder="Email" :required="true"/>
+        <Input type="password" id="signInPassword" placeholder="Password" :required="true"/>
         <Button class="flex" title="Sign in" is-class="btn-primary" size="btn-l"/>
       </form>
     </div>
   </div>
-
 </template>
 
 <script setup>
