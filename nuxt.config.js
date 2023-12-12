520a774b4d6844f922cd72096c0bb93d83f47482
young <young@spadecompany.kr>
2023-12-12 13:09:57 +0900

    chore: contents.css 적용

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,6 +20,7 @@ export default {
   css: [
     '~/assets/css/base.css',
     '~/assets/css/ui.css',
+    '~/assets/css/contents.css',
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
