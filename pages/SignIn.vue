af74308cd5193f7718c0a89549e944ec59f6c950
young <young@spadecompany.kr>
2023-12-12 13:14:40 +0900

    feat: SignIn 레이아웃 작업

--- pages/SignIn.vue
+++ pages/SignIn.vue
@@ -1,10 +1,24 @@
-<script setup>
-
-</script>
-
 <template>
+  <div class="container sign-in_container">
+    <div class="title_container">
+      <h1>Sign in</h1>
+      <nuxt-link to="/register">Need an account?</nuxt-link>
+    </div>
+    <div class="input_container">
+      <form>
+        <Input id="email" placeholder="Email" :required="true"/>
+        <Input id="password" placeholder="Password" :required="true"/>
+        <Button class="flex" title="Sign in" is-class="btn-primary" size="btn-l"/>
+      </form>
+    </div>
+  </div>
 
 </template>
+
+<script setup>
+import Input from "@/components/common/Input.vue";
+import Button from "@/components/common/Button.vue";
+</script>
 
 <style scoped>
 
