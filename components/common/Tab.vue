9c534f8b0e017733c7f6b82ccf9e1be469758c28
young <young@spadecompany.kr>
2023-12-12 13:54:08 +0900

    feat(Tab): 레이아웃 작업

---
+++ components/common/Tab.vue
@@ -0,0 +1,33 @@
+<template>
+  <ul class="flex tab_list">
+    <li class="tab_item" v-for="(list, index) in tabList" :key="index" :class="{is_active : list.isActive, is_show : list.isShow}">
+      <button>{{list.title}}</button>
+    </li>
+  </ul>
+</template>
+
+<script setup>
+import {ref} from "vue";
+
+const tabList = ref([
+  {
+    title: "Your Feed",
+    isShow: false,
+    isActive: false,
+  },
+  {
+    title: "Global Feed",
+    isShow: true,
+    isActive: true,
+  },
+  {
+    title: "",
+    isShow: false,
+    isActive: false,
+  }
+])
+</script>
+
+<style scoped>
+
+</style>
